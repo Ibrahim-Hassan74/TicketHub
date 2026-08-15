4004ff1e6892d81fb7640d5d62f348383ba9d0a3
Ibrahim-Hassan74 <ih3046376@gmail.com>
2026-08-15 20:28:15 +0300

    feat(routing): update ticket routes

--- src/app/features/tickets/tickets.routes.ts
+++ src/app/features/tickets/tickets.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
-export const TICKETS_ROUTES: Routes = [];
+export const TICKETS_ROUTES: Routes = [
+  {
+    path: '',
+    component: TicketListComponent
+  }
+];
